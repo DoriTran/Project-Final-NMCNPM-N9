8c8188bc9e36143d89484deb56d51164657d9037
DoriTran <tranquocdong308@gmail.com>
2021-12-17 21:37:01 +0700

    final

--- src/controllers/home_Controller.js
+++ src/controllers/home_Controller.js
@@ -5,17 +5,25 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+const KhoaHoc = require('../models/KhoaHoc');
+
 module.exports.renderHomeView = (req, res) => {
+    KhoaHoc.SortByRate();
+
+    KhoaByRate = KhoaHoc.all();
+
+    AnhKhoaHoc = [];
+
+    console.log(KhoaByRate);
+    //KhoaByRate = KhoaByRate.slice(0, 6);
 
     res.render('main', {
-        layout: 'main_layout'
+        layout: 'main_layout',
     });
 }
 
 module.exports.renderShowAllCourse = (req, res) => {
-
     res.render('all_courses', {
-
         layout: 'sub_layout'
     });
 };
